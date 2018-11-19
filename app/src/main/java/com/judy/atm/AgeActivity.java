0979f6d1a1b6b8fd4cec9707c3e5308218b3a438
Judy <aju452488@gmail.com>
2018-11-19 23:15:28 +0800

    '把它們補全'

--- app/src/main/java/com/judy/atm/AgeActivity.java
+++ app/src/main/java/com/judy/atm/AgeActivity.java
@@ -1,7 +1,10 @@
 package com.judy.atm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class AgeActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class AgeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+    }
+
+    public void next(View view){
+        EditText age = findViewById(R.id.ed_age);
+        int a = Integer.parseInt(age.getText().toString());
+        getSharedPreferences("user",MODE_PRIVATE).edit()
+                .putInt("age",a).apply();
+        Intent gender = new Intent(this,GenderActivity.class);
+        startActivity(gender);
+    }
+    public void back(View view){
+    finish();
     }
 }
--- app/src/main/java/com/judy/atm/GenderActivity.java
+++ app/src/main/java/com/judy/atm/GenderActivity.java
@@ -1,7 +1,10 @@
 package com.judy.atm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class GenderActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class GenderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gender);
+    }
+
+    public void next(View view){
+        EditText gender =findViewById(R.id.ed_gender);
+        int g = Integer.parseInt(gender.getText().toString());
+        getSharedPreferences("user",MODE_PRIVATE).edit()
+                .putInt("gender",g).apply();
+        Intent main = new Intent(this,MainActivity.class);
+        startActivity(main);
+    }
+    public void back(View view){
+        finish();
     }
 }
