822200378098c476cafe9083325eb2cd106ce3c9
Judy <aju452488@gmail.com>
2019-01-07 22:20:02 +0800

    'FinalExamPractice1'

---
+++ app/src/main/java/com/judy/atm/Login2Activity.java
@@ -0,0 +1,49 @@
+package com.judy.atm;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login2Activity extends AppCompatActivity {
+
+
+    private EditText password;
+    private EditText userid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+        //Refactor-Extract-method
+        userid = findViewById(R.id.ed_id);
+        password = findViewById(R.id.ed_pass);
+
+    }
+
+    public void login(View view){
+        String ui = userid.getText().toString();
+        String ps= password.getText().toString();
+        if(ui.equals("Judy")&&ps.equals("1234")){
+            Toast.makeText(this,"登入成功",Toast.LENGTH_LONG).show();
+            setResult(RESULT_OK);
+            finish();
+
+        }else{
+            new AlertDialog.Builder(this).setMessage("登入失敗").setTitle("ATM")
+                    .setPositiveButton("Ok",null)
+                    .setNegativeButton("Clear", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            userid.setText("");
+                            password.setText("");
+                        }
+                    }).show();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/judy/atm/Main2Activity.java
@@ -0,0 +1,32 @@
+package com.judy.atm;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+    boolean login = false;
+    public static final int RC_login =50;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        if(!login){//  if 沒登入,就進入loginActivty
+            Intent intent = new Intent(this, Login2Activity.class);
+            startActivityForResult(intent,RC_login);
+
+        }
+    }
+
+    // ctrl+o
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RC_login){
+            if(resultCode!=RESULT_OK){
+                finish();
+            }
+        }
+    }
+}
