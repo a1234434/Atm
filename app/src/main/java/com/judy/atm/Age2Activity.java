14dc1483dc641a6e2f65802bcc43c3c4f771bd14
Judy <aju452488@gmail.com>
2019-01-08 01:52:14 +0800

    'FinalExamPractice5'

--- app/src/main/java/com/judy/atm/Age2Activity.java
+++ app/src/main/java/com/judy/atm/Age2Activity.java
@@ -1,32 +1,74 @@
 package com.judy.atm;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Age2Activity extends Base2Activity {
-
+    private int number[]={18,19,20,21,22,23,24};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age2);
+        RecyclerView recyclerView =findViewById(R.id.rec);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
     public void next(View view){
-        EditText agee=findViewById(R.id.ed_ag);
-        int ageee =Integer.parseInt(agee.getText().toString());
-//        getSharedPreferences("user",MODE_PRIVATE).edit().
-//                putInt("age",ageee).apply();
-        user2.setAge(ageee);
-        Intent intent1 =new Intent(this,Gender2Activity.class);
-        startActivity(intent1);
+//        EditText agee=findViewById(R.id.ed_ag);
+//        int ageee =Integer.parseInt(agee.getText().toString());
+////        getSharedPreferences("user",MODE_PRIVATE).edit().
+////                putInt("age",ageee).apply();
+//        user2.setAge(ageee);
+//        Intent intent1 =new Intent(this,Gender2Activity.class);
+//        startActivity(intent1);
     }
 
     public void back(View view){
         finish();
 //        Intent intent =new Intent(this,Nickname2Activity.class);
 //        startActivity(intent);
+    }
+    class Age2Adapter extends RecyclerView.Adapter<Age2holder>{
+
+        @NonNull
+        @Override
+        public Age2holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view =getLayoutInflater().inflate(R.layout.age_row2,parent,false);
+            return new Age2holder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull Age2holder age2holder, int position) {
+            age2holder.ageText2.setText(number[position]+" ");
+            if(number[position]==19){
+                age2holder.ageText2.setTextColor(Color.RED);
+            }
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return number.length;
+        }
+    }
+
+    class Age2holder extends RecyclerView.ViewHolder{
+        TextView ageText2;
+
+        public Age2holder(@NonNull View itemView) {
+            super(itemView);
+            ageText2=findViewById(R.id.tv_age2);
+        }
     }
 }
--- app/src/main/java/com/judy/atm/Main2Activity.java
+++ app/src/main/java/com/judy/atm/Main2Activity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,41 +30,49 @@ public class Main2Activity extends Base2Activity {
             Intent intent = new Intent(this, Login2Activity.class);
             startActivityForResult(intent,RC_login);
 
+            //RecyclerView
+            RecyclerView recyclerView =findViewById(R.id.rec);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new FruitAdapter());
+
         }
     }
 //listView
     private void listView(){
         ArrayAdapter<String> adapter= new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,fruits);
-        ListView listView = findViewById(R.id.ed_list);
+        ListView listView = findViewById(R.id.list);
         listView.setAdapter(adapter);
     }
-//
-//    class Fruitadapter extends RecyclerView.Adapter<>{
-//
-//        @NonNull
-//        @Override
-//        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//            View view =LayoutInflater.from(parent.getContext()).
-//                    inflate(android.R.layout.simple_list_item_1,parent,false);
-//            return new FruitViewHolder(view);
-//        }
-//
-//        @Override
-//        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-//
-//        }
-//
-//        @Override
-//        public int getItemCount() {
-//            return 0;
-//        }
-//    }
-//    class FruitViewHolder extends RecyclerView.ViewHolder{
-//    TextView nameText;
-//    public FruitViewHolder(View itemview){
-//        super(itemView);
-//    }
-//    }
+    class FruitAdapter extends RecyclerView.Adapter<FruitViewHolder>{
+
+        @NonNull
+        @Override
+        public FruitViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view =LayoutInflater.from(parent.getContext()).
+                 inflate(android.R.layout.simple_list_item_1,parent,false);
+          return new FruitViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull FruitViewHolder fruitholder, int poistion) {
+          fruitholder.nameText2.setText(fruits.get(poistion));
+        }
+
+        @Override
+        public int getItemCount() {
+            return fruits.size();
+        }
+    }
+
+    class FruitViewHolder extends RecyclerView.ViewHolder {
+        TextView nameText2;
+    public FruitViewHolder(@NonNull View itemView) {
+        super(itemView);
+        nameText2=itemView.findViewById(R.id.text1);
+    }
+}
+
     // ctrl+o
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -73,7 +82,7 @@ public class Main2Activity extends Base2Activity {
                 finish();
             }else{
                 login=true;
-                if(user2.isVailed()) {
+                if(!user2.isVailed()) {
                     Intent intent = new Intent(this, Nickname2Activity.class);
                     startActivity(intent);
                 }
